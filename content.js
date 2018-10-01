caea8cbe44e0771202fcc96c09421801caa151f7
Evgeny Shiryaev <evgeny.shiryaev@devpool.com>
2018-10-01 21:23:05 +0300

    add custom rpc

--- content.js
+++ content.js
@@ -1,13 +1,32 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 
-const provider = new HDWalletProvider(
-        'saddle east game inmate screen defy alone embody cupboard grocery damage drive',
-        'https://rinkeby.infura.io/v3/ac236de4b58344d88976c12184cde32f');
-const web3 = new Web3(provider);
+let provider;
+let web3;
 
 const account = document.getElementById('account');
 const balance = document.getElementById('balance');
+
+
+const connect = async () => {
+    const networks = {
+            rinkeby: 'ac236de4b58344d88976c12184cde32f'
+    };
+
+    const network = document.getElementById('network').value;
+    let networkUri;
+    if (networks[network]) {
+        networkUri = 'https://rinkeby.infura.io/v3/' + networks[network];
+    } else {
+        // TODO
+        networkUri = 'http://localhost:8501';
+    }
+
+    provider = new HDWalletProvider(document.getElementById('mnemonic'), networkUri);
+    web3 = new Web3(provider);
+
+    document.getElementById('connected').innerHTML = 'connected';
+};
 
 
 const getBalance = async () => {
@@ -16,6 +35,9 @@ const getBalance = async () => {
 };
 
 
+document.getElementById('connect').addEventListener("click", function() {
+    connect();
+}, false);
 document.getElementById('get-balance').addEventListener("click", function() {
     getBalance();
 }, false);
