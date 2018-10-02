1b5c3ab3b10f9e3b99e6bf410ef5613cb9dcf1c3
Evgeny Shiryaev <evgeny.shiryaev@devpool.com>
2018-10-02 13:32:05 +0300

    ready

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# browser-wallet
+# Google Chrome Ethereum Wallet
+  - Mnemonic pharase
+  - Connection to real and test Ethereum netwoks
+  - Connection to node via RPC
+  - Get balance
+  - TODO: send transaction
 
-### Installation
-
-Requires [Node.js](https://nodejs.org/) to run.
-
-```sh
-$ npm install
-$ npm install -g browserify
-$ browserify content.js -o bundle.js
+## Installation
+### Software Prerequisites
+  - Install [Node.js](https://nodejs.org/)
+  - Run *npm install -g browserify*
+  - Run *npm install*
+### Building
+  - Run *browserify content.js -o bundle.js*
--- content.js
+++ content.js
@@ -1,43 +1,75 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 
+
+const MNEMONIC_ID = 'mnemonic';
+const CUSTOM_ID = 'custom';
+
+const mnemonicInput = document.getElementById(MNEMONIC_ID);
+const customInput = document.getElementById(CUSTOM_ID);
+
+
 let provider;
 let web3;
 
-const account = document.getElementById('account');
-const balance = document.getElementById('balance');
+
+const init = () => {
+    const mnemonic = localStorage.getItem(MNEMONIC_ID);
+    if (mnemonic) {
+        mnemonicInput.value = mnemonic;
+    }
+
+    const custom = localStorage.getItem(CUSTOM_ID);
+    if (custom) {
+        customInput.value = custom;
+    }
+}
 
 
 const connect = async () => {
+    // TODO: validate mnemonic
+    const mnemonic = mnemonicInput.value;
+    if (mnemonic === '') {
+        alert('mnemonic is not valid');
+        return;
+    }
+    localStorage.setItem(MNEMONIC_ID, mnemonic);
+
     const networks = {
             rinkeby: 'ac236de4b58344d88976c12184cde32f'
     };
-
     const network = document.getElementById('network').value;
     let networkUri;
     if (networks[network]) {
         networkUri = 'https://rinkeby.infura.io/v3/' + networks[network];
+    } else if (network == 'custom') {
+        networkUri = customInput.value;
+        localStorage.setItem(CUSTOM_ID, customInput.value);
     } else {
-        // TODO
-        networkUri = 'http://localhost:8501';
+        alert("Unknown network");
+        return;
     }
+    console.log('Connecting to ', networkUri);
 
-    provider = new HDWalletProvider(document.getElementById('mnemonic'), networkUri);
+    provider = new HDWalletProvider(mnemonic, networkUri);
     web3 = new Web3(provider);
 
     document.getElementById('connected').innerHTML = 'connected';
+
+    document.getElementById('account').value = (await web3.eth.getAccounts())[0];
+    getBalance();
 };
 
 
 const getBalance = async () => {
-    const result = await web3.eth.getBalance(account.value);
-    balance.innerHTML = web3.utils.fromWei(result, 'ether');
+    const result = await web3.eth.getBalance(document.getElementById('account').value);
+    document.getElementById('balance').innerHTML = web3.utils.fromWei(result, 'ether');
 };
 
 
 document.getElementById('connect').addEventListener("click", function() {
     connect();
 }, false);
-document.getElementById('get-balance').addEventListener("click", function() {
-    getBalance();
-}, false);
+
+
+init();
